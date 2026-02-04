949eb6b1e543b0600e5fdc8b6aa8183261dc73d8
michael angelo garcia <garciacruzmichaelangelo@gmail.com>
2026-02-04 09:12:24 +0800

    add stylesheet for buttons

--- application/views/dashboard/templates/tp_header.php
+++ application/views/dashboard/templates/tp_header.php
@@ -15,6 +15,7 @@
   <link class="js-stylesheet" href="/assets/css/light.css" rel="stylesheet" />
   <script src="/assets/js/settings.js"></script>
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">  
   
 
 <body
